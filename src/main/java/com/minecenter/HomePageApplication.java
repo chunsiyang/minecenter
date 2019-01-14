bea1a47e07366c3fbae8071102d5a0952a0b9cac
chunsi.yang@hotmail.com <chunsi.yang@hotmail.com>
2019-01-14 14:53:19 +0800

    依赖修改，tomcat支持修改

--- src/main/java/com/minecenter/HomePageApplication.java
+++ src/main/java/com/minecenter/HomePageApplication.java
@@ -2,11 +2,18 @@ package com.minecenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.minecenter.mapper")
-public class HomePageApplication {
+public class HomePageApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HomePageApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HomePageApplication.class, args);
